af568cfeb3a7b4cc0147204718fca3f6cc7b72b2
sai <45537954+sai5714@users.noreply.github.com>
2022-06-24 07:59:11 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -22,3 +22,16 @@ resource "aws_instance" "app_server" {
     Name = "ExampleAppServerInstance"
   }
 }
+
+resource "aws_s3_bucket" "test bucket" {
+  bucket = "s3-website-test.hashicorp.com"
+  acl    = "public-read"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["https://s3-website-test.hashicorp.com"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
