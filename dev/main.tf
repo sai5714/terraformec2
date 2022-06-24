8773316babf73d15722e1affcc683d9ab425933a
sai <45537954+sai5714@users.noreply.github.com>
2022-06-24 09:09:24 -0400

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -22,3 +22,12 @@ resource "aws_instance" "app_server" {
     Name = "ExampleAppServerInstance"
   }
 }
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
